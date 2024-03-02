1ba49634955e79bb81eb013f9ab73debc430bc01
Arne Kreikebaum <Arnekreikebaum@gmail.com>
2024-03-02 17:28:28 +0100

    updated the navbar with new links and routes

--- klausurenplan/resources/views/layouts/navigation.blade.php
+++ klausurenplan/resources/views/layouts/navigation.blade.php
@@ -13,7 +13,16 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Klausurplan') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('upload-csv')" :active="request()->routeIs('upload-csv')">
+                        {{ __('CSV hochladen') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('create-room')" :active="request()->routeIs('create-room')">
+                        {{ __('Raum hinzufügen') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('create-supervisor')" :active="request()->routeIs('create-supervisor')">
+                        {{ __('Aufsichtsperson hinzufügen') }}
                     </x-nav-link>
                 </div>
             </div>
