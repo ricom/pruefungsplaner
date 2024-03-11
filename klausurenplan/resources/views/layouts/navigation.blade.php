46965361b6355119e21e5756743322fbd402ef82
Arne Kreikebaum <Arnekreikebaum@gmail.com>
2024-03-11 15:51:59 +0100

    added Dropdown for Room and Supervisior

--- klausurenplan/resources/views/layouts/navigation.blade.php
+++ klausurenplan/resources/views/layouts/navigation.blade.php
@@ -11,19 +11,56 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex items-center">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Klausurplan') }}
                     </x-nav-link>
+
                     <x-nav-link :href="route('upload-csv')" :active="request()->routeIs('upload-csv')">
                         {{ __('CSV hochladen') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('create-room')" :active="request()->routeIs('create-room')">
-                        {{ __('Raum hinzufügen') }}
-                    </x-nav-link>
-                    <x-nav-link :href="route('create-supervisor')" :active="request()->routeIs('create-supervisor')">
-                        {{ __('Aufsichtsperson hinzufügen') }}
-                    </x-nav-link>
+
+                    <x-dropdown width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                {{ __( 'Räume') }}
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+
+                            <x-dropdown-link :href="route('create-room')">
+                                {{ __('Raum hinzufügen') }}
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('create-room')">
+                                {{ __('Raum ändern') }}
+                            </x-dropdown-link>
+
+                        </x-slot>
+
+                    </x-dropdown>
+
+                    <x-dropdown width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                {{ __( 'Aufsichtspersonen') }}
+                            </button>
+                        </x-slot>
+
+                        <x-slot name="content">
+
+                            <x-dropdown-link :href="route('create-supervisor')">
+                                {{ __('Aufsichtspersonen hinzufügen') }}
+                            </x-dropdown-link>
+
+                            <x-dropdown-link :href="route('create-supervisor')">
+                                {{ __('Aufsichtspersonen ändern') }}
+                            </x-dropdown-link>
+
+                        </x-slot>
+
+                    </x-dropdown>
                 </div>
             </div>
 
